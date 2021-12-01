ec435f6aaffee6244b9128b05304d41c10abe36b
Bernardo Trindade de Abreu <bernardo-trindade@hotmail.com>
2021-12-01 13:43:17 -0300

    workflow to publish package #49

--- packages/components/src/Button/Button.js
+++ packages/components/src/Button/Button.js
@@ -1,10 +1,9 @@
-import React from 'react';
+import React from 'react'
 
 const buttonStyle = {
-    padding: '10px 20px'
+  padding: '10px 20px'
 };
 
 const Button = ({ label }) => <button style={buttonStyle}>{label}</button>;
 
 export default Button;
-

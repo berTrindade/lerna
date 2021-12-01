8e42efb258974d8bb247067747f3b629ec253546
Bernardo Trindade de Abreu <bernardo-trindade@hotmail.com>
2021-12-01 12:31:29 -0300

    workflow to publish package #48

--- packages/components-ui/__tests__/components-ui.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const componentsUi = require('..');
-
-describe('components-ui', () => {
-    it('needs tests');
-});
--- packages/components-ui/lib/components-ui.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = componentsUi;
-
-function componentsUi() {
-    // TODO
-}

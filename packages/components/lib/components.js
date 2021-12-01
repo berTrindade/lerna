0d7af5e9937ff89ef4de1de6fba79e14925f70cf
Bernardo Trindade de Abreu <bernardo-trindade@hotmail.com>
2021-12-01 17:25:44 -0300

    workflow to publish package #55

--- packages/components/lib/components.js
+++ packages/components/lib/components.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const buttonStyle = {
- padding: '10px 20px'
+    padding: '10px 20px'
 };
 
 const Button = ({ label }) => <button style={buttonStyle}>{label}</button>;

6a420bdbc6826ea35cd2826f8acfcd0fc4ddf4c1
Bernardo Trindade de Abreu <bernardo-trindade@hotmail.com>
2021-12-02 15:54:34 -0300

    workflow to publish package #65

--- packages/components/lib/Button/index.js
+++ packages/components/lib/Button/index.js
@@ -1,24 +1,5 @@
 import React from "react";
 
-const Button = ({ onClick, children }) => {
-  return (
-    <>
-      <button className="button" onClick={onClick}>{children}</button>
-      <style jsx="true">
-        {`
-          .button  {
-            margin: 20px 0;
-            background-color: #0070f3;
-            border: 0;
-            cursor: pointer;
-            color: #FFFFFF;
-            font-size: 1.3em;
-            padding: 8px 16px;
-          }
-        `}
-        </style>
-    </>
-  );
-};
+const Button = ({ onClick, label }) => <button onClick={onClick}>{label}</button>;
 
 export default Button;

a5d9a928eacfd1c5e66638206a4f9792ede405e9
Bernardo Trindade de Abreu <bernardo-trindade@hotmail.com>
2021-12-05 22:47:46 -0300

    workflow to publish package #94

--- packages/components/lib/Modal/FloatingPage/FloatingPagePortal/index.js
+++ packages/components/lib/Modal/FloatingPage/FloatingPagePortal/index.js
@@ -2,10 +2,17 @@
 import { jsx } from "@emotion/core";
 import { Text } from "nectar-ui";
 import { createPortal } from "react-dom";
-import { Overlay } from "../style";
+import { Overlay, WrapContent } from "../style";
 
 export const FloatingPagePortal = ({ children }) => { 
-  return createPortal(<Overlay><Text>{children}</Text></Overlay>, document.body);
+  return createPortal(
+  <Overlay>
+    <WrapContent>
+      {children}
+    </WrapContent>
+    </Overlay>,    
+    document.body
+  );
 }
 
 export default FloatingPagePortal;
